63e01790f15344dff5463d75bd7b89582c011333
amulya <amulyagaur111@gmail.com>
2019-11-05 01:37:29 +0530

    Making UI changes for authentication.

--- healthcenter/views.py
+++ healthcenter/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # Create your views here.
 
 def index(request):
-    return render(request, 'healthcenter/signin.html')
+    print()
+    return render(request, 'healthcenter/welcome.html')
 
-def signin(request):
-    print(request.POST['username'])
-    print(request.POST['password'])
-    return render(request, 'healthcenter/signin.html')
+def dashboard(request):
+    if request.user.is_authenticated == True:
+        return render(request,'healthcenter/dashboard.html')
+    else:
+        return redirect('/hc')
+
+# def signin(request):
+#     print(request.POST['username'])
+#     print(request.POST['password'])
+#     return render(request, 'healthcenter/signin.html')
