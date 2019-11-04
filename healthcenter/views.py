84120024630601bc527686f445d7a3f0dfa70516
amulya <amulyagaur111@gmail.com>
2019-11-04 22:52:02 +0530

    Added login page

--- healthcenter/views.py
+++ healthcenter/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+def index(request):
+    return render(request, 'healthcenter/signin.html')
+
+def signin(request):
+    print(request.POST['username'])
+    print(request.POST['password'])
+    return render(request, 'healthcenter/signin.html')
