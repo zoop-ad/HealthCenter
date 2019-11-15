1a87dab240277a143e3f9ae3a473e5cf68b7b43d
amulya <amulyagaur111@gmail.com>
2019-11-16 01:14:21 +0530

    Fix default date selection in OPD reg

--- MNNIT_Healthcenter/settings.py
+++ MNNIT_Healthcenter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9yys*wo7$v8)+i=_#kp(+y$$9#sz89p&#9qsx3rywq^x)g0p&l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.29.34.139','localhost']
 
 
 # Application definition
