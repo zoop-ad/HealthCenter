a6f9086722e9419dc80d81978a7b2e5f96e74db0
amulya <amulyagaur111@gmail.com>
2020-05-13 08:48:10 +0530

    Reverting changes

--- healthcenter/views.py
+++ healthcenter/views.py
@@ -40,8 +40,7 @@ def patient_registration(request):
 def register_patient(request):
     np = Patient(cardNo=request.POST['cardno'],name=request.POST['fname'],dob=request.POST['dob'],contact_no=request.POST['cno'],address=request.POST['addr'],validity=request.POST['validity'],sex=request.POST['sex'],blood_grp=request.POST['bg'],emailid=request.POST['eml'],password=request.POST['eml'])
     np.save()
-    art = NewsArticle.objects.all()
-    return render(request,'healthcenter/front.html',{'msg':'Patient ' +request.POST['cardno']+' is successfully registered.','art':art})
+    return render(request,'healthcenter/front.html',{'msg':'Patient ' +request.POST['cardno']+' is successfully registered.'})
 
 def opdreg(request):
     doc_list = Doctor.objects.all()
@@ -67,8 +66,7 @@ def regopd(request):
     print(tim)
     reg = OPDRegistration(patient=pat,appoint_date=request.POST['dateofreg'],doctor=doc,checked=False)
     reg.save()
-    art = NewsArticle.objects.all()
-    return render(request,'healthcenter/front.html',{'art':art,'msg':'Successfully Registered for OPD on '+request.POST['dateofreg']+' with Dr. '+ doc.emp.first_name + ' '+doc.emp.last_name})
+    return render(request,'healthcenter/front.html',{'msg':'Successfully Registered for OPD on '+request.POST['dateofreg']+' with Dr. '+ doc.emp.first_name + ' '+doc.emp.last_name})
 
 def medavail(request):
     return render(request,'healthcenter/medavail.html')
@@ -169,8 +167,7 @@ def distributemed(request):
     mq =zip(meds,qty)
     html_message = render_to_string('healthcenter/receipt.html',{'dgs':dgs,'pat':dgs.patient,'mq':mq})
     send_mail("MNNIT Health Center OPD Receipt","",'amulya@mnnit.ac.in',[str(dgs.patient.emailid)],fail_silently=False,html_message=html_message)
-    art = NewsArticle.objects.all()
-    return render(request,'healthcenter/front.html',{'art':art,'msg':'Medicine Distributed successfully!'})
+    return render(request,'healthcenter/front.html',{'msg':'Medicine Distributed successfully!'})
 
 def verifyOTP(request):
     fbid = request.GET['fbid']
